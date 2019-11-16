67f9cdee4f9056906d5365cdb1d64f24ec9a428e
mher20030606 <mher.simonyan.2014@bk.ru>
2019-11-16 17:08:00 +0400

    Updates

---
+++ precache-manifest.3a177324c06b2002438ebe77178c23ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cbea0d545e81374d8a0b523884cc29",
+    "url": "/BitMoney/index.html"
+  },
+  {
+    "revision": "cd6d2487d3937d0ec436",
+    "url": "/BitMoney/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "f28f28cda5a319c14fd9",
+    "url": "/BitMoney/static/js/2.da359006.chunk.js"
+  },
+  {
+    "revision": "cd6d2487d3937d0ec436",
+    "url": "/BitMoney/static/js/main.9d9314a5.chunk.js"
+  },
+  {
+    "revision": "e9850d4344a692b51236",
+    "url": "/BitMoney/static/js/runtime-main.c72315fe.js"
+  }
+]);
